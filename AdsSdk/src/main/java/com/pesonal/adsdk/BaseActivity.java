53c1630dace00119fd74c5a7a1a9c8f5c37deb10
unknown <ravinasukhadiya1998@gmail.com>
2022-01-25 11:21:12 +0530

    update

--- AdsSdk/src/main/java/com/pesonal/adsdk/BaseActivity.java
+++ AdsSdk/src/main/java/com/pesonal/adsdk/BaseActivity.java
@@ -33,11 +33,7 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     public int hidesystembar() {
-        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
+        return View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
     }
 }
